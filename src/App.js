379f508502c65260a363173040d3f282e48b9f97
michael sodovski <michaelsod90@gmail.com>
2022-05-15 00:02:05 +0300

    register, payment, navigationBar, gihnIn components added and modified

--- src/App.js
+++ src/App.js
@@ -4,24 +4,33 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../src/components/home/home.component'
 import Menu from '../src/components/menu/menu.component';
 import Register from '../src/components/register/register.component';
+import NavigationBar from '../src/components/navigationBar/navigationBar.component';
+import SignIn from '../src/components/signIn/signIn.component'
+import Users from '../src/components/users/users.component'
 
 export default function App() {
     return (
         <div className="App">
             <header className="App-header">
-                
+                <NavigationBar></NavigationBar>
             </header>
             <div className="layout-flexbox-container">
                 <aside className="App-right-side_menu">
-
+                    <Menu></Menu>
                 </aside>
                 <main className="App-main">
                     <Switch>
-                        <Route path="/Home">
+                        <Route path="/home">
                             <Home></Home>
                         </Route>
-                        <Route path="/Register">
+                        <Route path="/register">
                             <Register></Register>
+                        </Route>
+                        <Route path="/signin">
+                            <SignIn></SignIn>
+                        </Route>
+                        <Route path="/users">
+                            <Users></Users>
                         </Route>
                     </Switch>
                 </main>
