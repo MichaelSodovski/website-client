46f078b6d1bed83103ac458d9568b69892238322
michael sodovski <michaelsod90@gmail.com>
2022-06-03 12:37:06 +0300

    sign in form updated basic architecture and css styling

--- src/components/signIn/signIn.component.jsx
+++ src/components/signIn/signIn.component.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import '../signIn/signIn.component.css'
-import { Link } from 'react-router-dom';
 
 export default function SignIn() {
 
     return (
         <React.Fragment>
-            <div className="signIn-main-container">
+            <form action="/validateUser" method="POST">
                 <div className="signIn-form-container">
-                    <label>User Name: </label>
-                    <input></input>
-                    <br></br>
-                    <label>Password: </label>
-                    <input></input>
-                    <br></br>
-                    <Link>
-                        <button to="/home">Sign in</button>
-                    </Link>
+                    <div className="userName-form-container"> 
+                        <input placeholder='User name'></input>
+                    </div>
+                    <div className="password-form-container">
+                        <input placeholder='Password'></input>
+                    </div>
+                    <div class="forgot-remember-container">
+                        <input type="checkbox"></input>
+                        <a>Forgot password?</a>
+                    </div>
+                    <div className="form-submit-btn-container">
+                        <button className="form-submit-btn" type="submit" value="Submit">Sign in</button>
+                    </div>
+                    <div class="sign-up-container">
+                        <a>Don't have an account?</a>
+                        <a to="/register">sign up</a>
+                    </div>
+                    <hr></hr>
                 </div>
-            </div>
+            </form>
         </React.Fragment>
     );
 }
